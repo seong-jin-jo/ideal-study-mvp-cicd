822134adae6abc10368c981b798fee94e9d0b9ee
jin66 <dbf6609@naver.com>
2024-11-11 16:00:08 +0900

    refacotr(SecuirtyConfig.java) : cors 설정

--- src/main/java/com/idealstudy/mvp/config/SecurityConfig.java
+++ src/main/java/com/idealstudy/mvp/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.idealstudy.mvp.config;
 
 import com.idealstudy.mvp.enums.member.Role;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +32,11 @@ public class SecurityConfig {
         // The configuration below requires authentication to every URL and will grant access
         // to both the user "admin" and "user".
         http.authorizeHttpRequests(auth ->
-            auth.requestMatchers("/*").permitAll()
+            auth
+                    .requestMatchers("/*").permitAll()
+                    // 정적 파일 허용
+                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                    
                     .requestMatchers("/student/**").hasRole(Role.STUDENT.getRoleStr())
                     .requestMatchers("/teacher/**").hasRole(Role.TEACHER.getRoleStr())
                     .requestMatchers("/admin/**").hasRole(Role.ADMIN.getRoleStr())
@@ -47,6 +56,22 @@ public class SecurityConfig {
         // Enables CSRF protection. This is activated by default when using EnableWebSecurity.
         // CSRF token 사용 시 POST 요청만 가능.
         http.csrf(csrf -> csrf.disable());
+
+        // CORS 활성화
+        http.cors(customizer -> customizer.configurationSource( // HttpServletRequest를 기반으로 CORS 설정을 반환
+                new CorsConfigurationSource() { // CORS 설정을 직접 정의하는 익명 클래스
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) { // CORS 정책을 정의
+                        CorsConfiguration config = new CorsConfiguration(); // corsConfiguration 객체를 생성하여 CORS 설정을 담을 컨테이너로 사용
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); // CORS 요청을 허용할 출처
+                        config.setAllowedMethods(Collections.singletonList("*")); // CORS 요청을 허용할 메서드
+                        config.setAllowCredentials(true); // CORS 쿠키나 인증정보를 포함한 요청 허용
+                        config.setAllowedHeaders(Collections.singletonList("*")); // CORS 요청을 허용할 헤더
+                        config.setMaxAge(3600L); // pre-flight 요청을 캐싱할 1시간
+                        return config;
+                    }
+                }
+        ));
 
         // This is automatically applied when using EnableWebSecurity.
         // The default is that accessing the URL "/logout" will log the user out by invalidating the HTTP Session,
