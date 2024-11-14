4d9f5897abe5d52ab114f9a6f6439bd124acdd0e
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2024-11-14 23:15:50 +0900

    feat: JWT utility 구현

--- src/main/java/com/idealstudy/mvp/enums/member/Role.java
+++ src/main/java/com/idealstudy/mvp/enums/member/Role.java
@@ -1,5 +1,8 @@
 package com.idealstudy.mvp.enums.member;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Role {
 
     // It should not start with "ROLE_"
@@ -7,24 +10,29 @@ public enum Role {
     TEACHER("TEACHER"),
     ADMIN("ADMIN");
 
+    private static final Map<String, Role> ENUM_STRING = new HashMap<>();
+
+    static {
+        for (Role role : values()) {
+            ENUM_STRING.put(role.role, role);
+        }
+    }
+
     private String role;
 
     Role(String role) {
         this.role = role;
     }
 
-    public String getRole() {
+    public String toString() {
         return role;
     }
 
-    /*
-    public static Role stringToEnum(String role) {
-        for(Role val : Role.values())
-            if(val.getRoleStr().equals(role))
-                return val;
-        
-        throw new IllegalArgumentException(MemberError.ROLL_MISS_MATCH.getMsg());
+    public static Role fromString(String text) {
+        Role role = ENUM_STRING.get(text.toUpperCase());
+        if (role == null) {
+            throw new IllegalArgumentException("No constant with text " + text + " found");
+        }
+        return role;
     }
-
-     */
 }
