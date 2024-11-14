638d7a8ca0cde69d39947bf9c1f2cc2b971f4e1b
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2024-11-15 01:08:58 +0900

    feat: 1~2주차에 해당하는 기능들에 대한 endpoint 추가

--- src/main/java/com/idealstudy/mvp/presentation/MemberController.java
+++ src/main/java/com/idealstudy/mvp/presentation/MemberController.java
@@ -1,9 +1,43 @@
 package com.idealstudy.mvp.presentation;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class MemberController {
 
+    @PostMapping("/auth/login")
+    public void login() {
 
+    }
+
+    @PostMapping("/auth/logout")
+    public void logout() {
+
+    }
+
+    @PostMapping("/api/users/sign-up")
+    public void signUp() {
+
+    }
+
+    @GetMapping("/api/users/{userId}")
+    public void findMember(@PathVariable String userId) {
+
+    }
+
+    // page number는 어떻게 표현할 생각?
+    @GetMapping("/api/users")
+    public void findMemberList() {
+
+    }
+
+    @DeleteMapping("/api/users/{userId}")
+    public void deleteMember(@PathVariable String userId) {
+
+    }
+
+    @PatchMapping("/api/users/{userId}")
+    public void updateMember(@PathVariable String userId) {
+
+    }
 }
