a9d84ac0c49064b574f8c8ec00bf6cfdedf11b13
SEONG JIN JO <123255935+seong-jin-jo@users.noreply.github.com>
2024-11-26 01:32:54 +0900

    feat : 유저 더미데이터 생성 버튼 추가(HomePage.js)

--- frontend/src/pages/home/HomePage.js
+++ frontend/src/pages/home/HomePage.js
@@ -1,12 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AuthContext } from '../../context/AuthContext';
+import { makeDummyUser } from '../../services/UserService.mjs';
+import Button from '../../components/Button';
 
 const HomePage = () => {
   const { isAuthenticated } = useContext(AuthContext);
 
+  const makeDummy = async() => {
+      await makeDummyUser(); 
+  }
+
   return (
     <div>
       <main style={{ padding: '10px' }}>
+        <Button onClick={makeDummy}> 더미유저 생성하기 </Button>
         <h1>메인 페이지</h1>
         {isAuthenticated ? (
           <div>
