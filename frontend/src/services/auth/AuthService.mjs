38e1a8cb2b0be6a0838b398f989532c6ef4d3de6
SEONG JIN JO <123255935+seong-jin-jo@users.noreply.github.com>
2024-12-30 12:49:22 +0900

    feat : 24.12.30 통합테스트 준비 - front 도커파일 + jwt토큰 헤더로 받기

--- frontend/src/services/auth/AuthService.mjs
+++ frontend/src/services/auth/AuthService.mjs
@@ -11,9 +11,15 @@ export const loginUser = async (username, password) => {
       password,
     });
     console.log("로그인 API 성공:", response);
-    // 서버에서 받은 JWT 토큰과 유저 정보 반환
-    const { token, user } = response.data; // 서버에서 반환된 데이터 구조에 맞게 수정 필요
-    return { token, user }; // 토큰과 유저 정보를 객체로 반환
+
+    // header로 받은 토큰 localStorage 저장
+    const token = response.headers["authorization"];
+    if (token) {
+      localStorage.setItem("jwtToken", token);
+    }
+
+    // body로 받은 유저이름 반환
+    return response.data;
   } catch (error) {
     console.log("로그인 API 실패", error);
 
